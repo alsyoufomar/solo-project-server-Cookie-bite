ed1d59a5bf69df02b7ae432103f4c6770eb80460
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-05-21 13:46:20 +0100

    featured and this week lists

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -1,41 +1,56 @@
 const prisma = require('../utils/prisma');
 
 const getEvent = async (req, res) => {
-  const { location, genre, startDate, endDate, cursorId, perPage } = req.query;
+  const { location, genre, startDate, endDate, page, perPage } = req.query;
 
-  const skip = 1;
+  const skip = parseInt(page);
   const take = parseInt(perPage);
+  console.log('page: ', take, skip);
   const where = {};
-  const cursor = {};
   if (location) where.location = location;
   if (genre) where.genre = genre;
   if (startDate || endDate) {
     where.createdAt = {
-      gte: new Date('18 Apr 2022'),
-      lt: new Date('28 Apr 2022'),
+      gte: new Date(startDate.substring(4, 15)),
+      lt: new Date(endDate.substring(4, 15)),
     };
   }
-  let test = 1;
-
-  if (cursorId != 0) {
-    test = parseInt(cursorId);
-  }
-
-  console.log(cursorId);
 
   const foundEvent = await prisma.$transaction([
     prisma.event.count({ where }),
     prisma.event.findMany({
       where,
-      skip,
       take,
-      cursor: {
-        id: test,
-      },
+      skip: skip * take,
     }),
   ]);
-
   res.json({ events: foundEvent });
 };
 
-module.exports = { getEvent };
+const getFeatured = async (req, res) => {
+  const featured = await prisma.event.findMany({
+    where: {
+      location: 'London',
+      genre: 'gigs',
+    },
+    skip: 13,
+    take: 20,
+  });
+
+  res.json({ events: featured });
+};
+
+const getThisWeek = async (req, res) => {
+  const thisWeek = await prisma.event.findMany({
+    where: {
+      location: 'Liverpool',
+      genre: 'club',
+    },
+    skip: 13,
+    take: 8,
+  });
+
+  res.json({ events: thisWeek });
+};
+
+module.exports = { getEvent, getFeatured, getThisWeek };
